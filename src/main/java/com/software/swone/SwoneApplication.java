063e205592091961400cdf21814b1a37bddf729e
BryanLR <108194629+bryanLRx@users.noreply.github.com>
2024-04-27 20:31:16 -0500

    chore: add first project files

---
+++ src/main/java/com/software/swone/SwoneApplication.java
@@ -0,0 +1,13 @@
+package com.software.swone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/software/swone/SwoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.swone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
